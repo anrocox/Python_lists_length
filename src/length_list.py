7da09fb8d4585fde276a73cdbd3ebd0a7a9d6284
Andres Rodriguez <anrocox@gmail.com>
2014-07-02 10:26:56 -0500

    How can you determine the size or length of a list in python?

---
+++ src/length_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+Created on Jul 1, 2014
+
+@author: anroco
+
+How can you determine the size or length of a list in python?
+
+¿Como se puede determinar el tamaño o longitud de una lista en python?
+'''
+
+#create a list with numbers
+lista = [1, 3, 8, 27, 64]
+print (lista)
+
+#know the length of a list, use the len() function of built-in python
+print (len(lista))
+
+#add a new item to the end of the list
+lista.append(125)
+print (lista)
+
+#know the length of a list
+print (len(lista))
